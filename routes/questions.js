f35c4b9dfb5ac632305b4f95b5b5d8d66a62e63c
Sean <sd.cotter45@gmail.com>
2021-07-27 13:01:37 -0400

    working on new questions routes

--- routes/questions.js
+++ routes/questions.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const csrf = require('csurf');
+
+const { Question, Answer, User, Upvote, Downvote } = require('../db/models');
+const { requireAuth } = require('../auth');
+const { asyncHandler, handleValidationErrors } = require('./util');
+
+
 const router = express.Router();
+const csrfProtection = csrf({ cookie: true })
 
+router.get('/ask', csrfProtection, requireAuth, handleValidationErrors, (req, res) => {
+    res.render('ask', {
+        csrfToken: req.csrfToken()
+    });
+});
 
+router.post('/ask', csrfProtection, requireAuth, handleValidationErrors, asyncHandler(async (req, res) => {
+    const { question, additional_info } = req.body;
+    const { userId } = req.session.auth;
 
+    if (userId) {
+        const newQuestion = await Question.create({
+            question,
+            additional_info,
+            userId
+        })
 
+        res.redirect('question', { newQuestion })
+    } else {
+        res.redirect('login');
+    }
+}))
 
 module.exports = router;
