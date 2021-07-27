9f473eb003b819203395c6d13492dbd58f44d618
zaneh <zanehamadi@gmail.com>
2021-07-27 12:58:36 -0400

    Created a questions route and display

--- routes/questions.js
+++ routes/questions.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const csrf = require('csurf');
+
+const { Question, Answer, User, Upvote, Downvote } = require('../db/models');
+const { requireAuth } = require('../auth');
+const { asyncHandler, handleValidationErrors } = require('./util');
+
+
 const router = express.Router();
+const csrfProtection = csrf({ cookie: true })
+
+
+// Render questions route(displays questions)
+
+router.get('/', asyncHandler(async (req,res,next) => {
+    const questions = await Question.findAll({
+        include: User
+    })
+
+    res.render('questions', {questions})
+}))
+
+
 
 
 
